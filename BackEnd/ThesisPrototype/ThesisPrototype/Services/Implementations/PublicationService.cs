73ff02200d8e21f1950f11fe5079860f55bd2ac3
Cas Fahrenfort <casfahrenfort@live.nl>
2019-11-29 11:48:38 +0100

    Disable publication db

--- BackEnd/ThesisPrototype/ThesisPrototype/Services/Implementations/PublicationService.cs
+++ BackEnd/ThesisPrototype/ThesisPrototype/Services/Implementations/PublicationService.cs
@@ -24,66 +24,22 @@ namespace ThesisPrototype.Services.Implementations
 
         public Publication FindDuplicatePublication(string checksum)
         {
-            List<Publication> pubs = publications.Find<Publication>(p => p.Checksum == checksum).ToList();
-            
-            if (pubs.Count == 0)
-            {
-                return null;
-            }
-            else
-            {
-                foreach(Publication p in pubs)
-                {
-                    if (p.Open_Access)
-                    {
-                        return p;
-                    }
-                }
-            }
-
             return null;
         }
 
         public PublicationBundle FindDuplicatePublicationBundle(List<Publication> publications)
         {
-            List<PublicationBundle> pubs = publicationBundles.Find<PublicationBundle>(p => true).ToList();
-
-            pubs = pubs.FindAll(p => IsDuplicateBundle(p, publications));
-
-
-            if (pubs.Count == 0)
-            {
-                return null;
-            }
-            else
-            {
-                foreach (PublicationBundle p in pubs)
-                {
-                    if (p.Open_Access)
-                    {
-                        return p;
-                    }
-                }
-            }
-
             return null;
         }
 
         private bool IsDuplicateBundle(PublicationBundle bundle, List<Publication> publications)
         {
-            foreach (Publication pub in publications)
-            {
-                if (!bundle.PublicationIds.Contains(pub.Id))
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            return false;
         }
 
         public Publication CreatePublication(string sourceUrl, string publicationUrl, bool open_access, string checksum)
         {
+
             Publication publication = new Publication()
             {
                 Checksum = checksum,
@@ -91,8 +47,6 @@ namespace ThesisPrototype.Services.Implementations
                 PublicationUrl = publicationUrl,
                 SourceUrl = sourceUrl
             };
-
-            publications.InsertOne(publication);
 
             return publication;
         }
@@ -106,8 +60,6 @@ namespace ThesisPrototype.Services.Implementations
                 PublicationUrl = publicationUrl,
                 Open_Access = open_access
             };
-
-            publicationBundles.InsertOne(publicationBundle);
 
             return publicationBundle;
         }
